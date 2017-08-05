b1ab8206cbe80471a134f08c7712f03441c574f8
Michael Beckwith <tw2113@gmail.com>
2017-08-05 12:43:29 -0500

    re-add our phpdoc blocks

--- classes/class-mb-untappd-api.php
+++ classes/class-mb-untappd-api.php
@@ -11,13 +11,35 @@
  */
 class MB_Untappd_API {
 
-
+	/**
+	 * Base Untappd API endpoint.
+	 *
+	 * @var string
+	 * @since 1.3.0
+	 */
 	protected $base_uri = 'https://api.untappd.com/v4/';
 
+	/**
+	 * Untappd API Client ID.
+	 *
+	 * @var mixed|string
+	 * @since 1.3.0
+	 */
 	protected $client_id = '';
 
+	/**
+	 * Untappd API Client Secret ID.
+	 *
+	 * @var mixed|string
+	 * @since 1.3.0
+	 */
 	protected $client_secret = '';
 
+	/**
+	 * MB_Untappd_API constructor.
+	 *
+	 * @param array $args
+	 */
 	public function __construct( $args = array() ) {
 		$this->client_id     = $args['client_id'];
 		$this->client_secret = $args['client_secret'];
