4bc5a79140af18d4849c67c31845136f86a5d13a
Michael Beckwith <tw2113@gmail.com>
2017-09-15 14:17:33 -0500

    new helper methods for fetching current limits on api requests and a message for rate limit met.

--- classes/class-mb-untappd-api.php
+++ classes/class-mb-untappd-api.php
@@ -45,4 +45,27 @@ class MB_Untappd_API {
 		$this->client_secret = $args['client_secret'];
 		$this->username      = $args['username'];
 	}
+
+	/**
+	 * Get our current rate limit from the Untappd API.
+	 *
+	 * @since 1.3.0
+	 *
+	 * @param $request WP HTTP API request.
+	 * @return string
+	 */
+	public function get_current_limit( $request ) {
+		return wp_remote_retrieve_header( $request, 'x-ratelimit-remaining' );
+	}
+
+	/**
+	 * Return a blanket rate limit met message.
+	 *
+	 * @since 1.3.0
+	 *
+	 * @return array
+	 */
+	public function get_rate_limit_met_message() {
+		return array( 'error' => esc_html__( 'Reached rate limit for current hour.', 'mb_untappd' ) );
+	}
 }
